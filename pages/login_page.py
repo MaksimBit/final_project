0e0c54de7f62b98f731e03ffc9ef0849a0169779
Максим Биткин <you@example.com>
2025-03-04 16:44:43 +0300

    between pages

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,13 +9,10 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
         assert "login" in self.browser.current_url ,  "URL не совпадает!"
 
     def should_be_login_form(self):
-        
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Формы для логина нет"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Формы для регистрации нет"
